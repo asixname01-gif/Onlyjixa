58f1334d44a56ed97197a618b494bf88e13a53c3
asixname01-gif <asixname01@gmail.com>
2026-03-10 04:16:46 +0430

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const http = require("http");
+import http from "http";
+
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("Server is running 🚀");
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Tunnel Server Running");
 });
 
-server.listen(process.env.PORT || 8080);
+server.listen(port, () => {
+  console.log("Server running on port " + port);
+});
